2fe9b89e7ef95c2b6021b5878af27d930f44610c
NOOB hacker raj <87321079+Temuxuser2021@users.noreply.github.com>
2021-12-26 13:15:59 +0530

    Update freeprefix3.js

--- plugins/freeprefix3.js
+++ plugins/freeprefix3.js
@@ -1,6 +1,6 @@
 module.exports = {
     before(m) {
-      if (/\b(wass up)\b/i.test(m.text)) m.reply('🎀 Nothing Am Here To Help You My Server Is Online🎀 ')
+      if (/\b(wass up)\b/i.test(m.text)) m.reply('🎀 Nothing Am Here To fuck You My Server Is Online🎀 ')
       return !0
     }
   }
