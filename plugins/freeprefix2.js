4163bc7700c24db67f1c90628a65e018f5f3d347
NOOB hacker raj <87321079+Temuxuser2021@users.noreply.github.com>
2021-12-26 13:15:16 +0530

    Update freeprefix2.js

--- plugins/freeprefix2.js
+++ plugins/freeprefix2.js
@@ -1,12 +1,12 @@
 module.exports = {
     before(m) {
-      if (/\b(hello)\b/i.test(m.text)) m.reply('🎀 Yes ZTech Bot Is Here🎀 ')
+      if (/\b(hello)\b/i.test(m.text)) m.reply('🎀 Yes Noob Hacker Bot Is Here🎀 ')
       return !0
     }
   }
   module.exports = {
     before(m) {
-      if (/\b(how are you)\b/i.test(m.text)) m.reply('🎀 Am Fine What About you🎀 ')
+      if (/\b(how are you)\b/i.test(m.text)) m.reply('🎀 me mast hu tu keisa hei wo bata 🎀 ')
       return !0
     }
   }
