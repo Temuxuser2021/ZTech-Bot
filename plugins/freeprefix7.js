782a8b5835ecbd5fd71ac42871a51b7bc12d5ed1
NOOB hacker raj <87321079+Temuxuser2021@users.noreply.github.com>
2021-12-26 13:18:27 +0530

    Update freeprefix7.js

--- plugins/freeprefix7.js
+++ plugins/freeprefix7.js
@@ -1,6 +1,6 @@
 module.exports = {
     before(m) {
-      if (/\b(bsdk)\b/i.test(m.text)) m.reply('Seems Like Your Mom And Dad Didnt Teached You Manners Probably They Also Use Such Words At Home And You Learnt It From Them🐦🐦')
+      if (/\b(bsdk)\b/i.test(m.text)) m.reply('chal be maderchod jyada ud mat ma chod denge tumhari 🐦🐦')
       return !0
     }
   }
